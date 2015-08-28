4e66def313599b246a506c1729d885947393d793
Kartik Yadav <kartik@Kartiks-MacBook-Pro.local>
2015-08-28 12:42:08 -0400

    Website front setup

--- web/website/app/js/src/component/controller.js
+++ web/website/app/js/src/component/controller.js
@@ -1,32 +1,5 @@
 var app = angular.module('myApp', ['ngRoute']);
 
-app.config(function($httpProvider) {
-    //Enable cross domain calls
-    $httpProvider.defaults.useXDomain = true;
-});
-
-app.factory('getCallsApi', ['$http', 'Base64', function($http, Base64) {
-    return {
-        get: function(callback) {
-            $http({
-                url: //Some REST get call,
-                method: "GET",
-                withCredentials: true,
-                headers: {
-                    ''
-                    'Content-Type': 'text/plain',
-                    'Authorization': 'Basic'
-                }
-            }).success(function(data) {
-                callback(data);
-            });
-            
-        }
-    }
-}]);
-
-app.controller('appController', ['$scope','getCallsApi', function($scope, getCallsApi) {
-  getCallsApi.get(function(data) {
-    console.log(data);
-  });
+app.controller('appController', ['$scope', function($scope) {
+  
 }]);
