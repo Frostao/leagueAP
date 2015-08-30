59b50af4a3af205c1d80a7ceee35a5960029af33
Kartik Yadav <kartik@Kartiks-MacBook-Pro.local>
2015-08-29 21:23:19 -0400

    Front end stuff

--- web/website/app/app.js
+++ web/website/app/app.js
@@ -1,3 +1,5 @@
+(function () {
+'use strict';
 var app = angular.module('leagueApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
@@ -20,34 +22,59 @@ app.config(function($routeProvider) {
 });
 
 app.controller('compareController', ['$scope', '$http', function($scope, $http) {
+    'use strict';
+    /*var ChampionData = {
+        name : null,
+        image: null,
+        id : 0,
+        freq : 0,
+        wins : 0,
+        kills : 0,
+        deaths : 0,
+        items : []
+    }*/
+    function ChampionData () {
+        this.name = null;
+        this.image = null;
+        this.id = 0;
+        this.freq = 0;
+        this.wins = 0;
+        this.kills = 0;
+        this.deaths = 0;
+        this.items = [];
+    }
     $scope.allData = []
     $scope.allChampions = {}
-    
+
     // create a message to display in our view
     $scope.getAllLeagueAPData = function() {
-        /*$http.get('http://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json').
+        $http.get('http://ddragon.leagueoflegends.com/cdn/5.2.1/data/en_US/champion.json').
           then(function(response) {
-            $scope.allChampions = response.data.data;
-            console.log($scope.allChampions);
+            $scope.allChampions = angular.fromJson(response.data.data);
+            angular.forEach($scope.allChampions, function(item){
+                var champ = new ChampionData();
+                champ.name = item.id;
+                champ.image = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/"+item.id+".png";
+                $http.get('http://localhost:7001/api/champion/id/'+item.key).
+                  then(function(response) {
+                    champ.id = response.data.id;
+                    champ.freq = response.data.freq;
+                    champ.wins = response.data.wins;
+                    champ.kills = response.data.kills;
+                    champ.deaths = response.data.deaths;
+                    champ.items = response.data.items;
+                    $scope.allData.push(champ);
+                  }, function(response) {
+                    console.log(response.data);
+                  }); 
+            })
           }, function(response) {
             console.log(response.data);
-          });
-
-        for (var i = 0; i< 10; i++) {
-            console.log(i);
-        }*/
-        $http.get('http://localhost:7001/api/all').
-          then(function(response) {
-
-            $scope.allData = response.data;
-            console.log($scope.allData);
-          }, function(response) {
-            console.log(response.data);
-          });
+          });    
     }
     $scope.getAllLeagueAPData();
 }]);
-
+//http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/Aatrox.png
 app.controller('authorsController', function($scope) {
 
 });
@@ -55,3 +82,4 @@ app.controller('authorsController', function($scope) {
 app.controller('splashController', function($scope) {
 
 });
+})();
